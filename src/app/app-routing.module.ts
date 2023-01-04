098a2fa085fe04351d16e0f3ca68a0ad219bdba2
albertdanl <a.danielqz@outlook.com>
2023-01-04 14:47:29 -0600

    Angular base body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+//Components
+import { HomeComponent } from './components/home/home.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contacts', component: ContactListComponent},
+  {path: '**',redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
